bae72c44f986bd48b7b3dcf77aad982466199eba
Maneesh Jain <maneesh.jain@samsung.com>
2015-11-20 11:53:36 +0530

    Merge:Partial Fix a crash and valgrind invalid read error
    Change-Id: Ib36cab79ea86048860ffda7d6108b769a3cc9b33
    Signed-off-by: Maneesh Jain <maneesh.jain@samsung.com>

--- gdhcp/client.c
+++ gdhcp/client.c
@@ -2776,7 +2776,7 @@ int g_dhcp_client_start(GDHCPClient *dhcp_client, const char *last_address)
 		if (addr == 0xFFFFFFFF || ((addr & LINKLOCAL_ADDR) ==
 					LINKLOCAL_ADDR)) {
 			addr = 0;
-		} else {
+		} else if (dhcp_client->last_address != last_address) {
 			g_free(dhcp_client->last_address);
 			dhcp_client->last_address = g_strdup(last_address);
 		}
