1e0637575d1d1250baa8551dde46bb7bab02bc38
Jaehyun Kim <jeik01.kim@samsung.com>
2018-01-12 18:38:45 +0900

    Handles the passphrase decryption failure case
    Change-Id: I8511bbc2b025f296770420dc9c8e7b85aa2be60d
    Signed-off-by: Jaehyun Kim <jeik01.kim@samsung.com>

--- gsupplicant/supplicant.c
+++ gsupplicant/supplicant.c
@@ -5540,11 +5540,13 @@ static void decryption_request_reply(DBusPendingCall *call,
 	if (dbus_set_error_from_message(&error, reply)) {
 		SUPPLICANT_DBG("decryption_request_reply() %s %s", error.name, error.message);
 		dbus_error_free(&error);
+		ret = -EINVAL;
 		goto done;
 	}
 
 	if (dbus_message_iter_init(reply, &args) == FALSE) {
 		SUPPLICANT_DBG("dbus_message_iter_init() failed");
+		ret = -EINVAL;
 		goto done;
 	}
 
@@ -5559,6 +5561,7 @@ static void decryption_request_reply(DBusPendingCall *call,
 		interface_add_network_result, data,
 		data->interface);
 
+done:
 	if (ret < 0) {
 		SUPPLICANT_DBG("AddNetwork failed %d", ret);
 		callback_assoc_failed(decrypt_request_data.data->user_data);
@@ -5567,7 +5570,6 @@ static void decryption_request_reply(DBusPendingCall *call,
 		dbus_free(data);
 	}
 
-done:
 	dbus_message_unref(reply);
 	dbus_pending_call_unref(call);
 
